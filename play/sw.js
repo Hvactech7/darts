d2767cfe1421a5e6146f6bf51d448fefc501fa78
Nathan Breeding <hvacbreeding@gmail.com>
2026-08-07 00:21:52 -0700

    Undo/Redo in 01 board overlay, cricket Back clear of scores, Start/Manage swapped (darts-v20)

--- play/sw.js
+++ play/sw.js
@@ -8,7 +8,7 @@
 // refresh, which is what keeps startup instant.
 //
 // Bump CACHE when the file list below changes.
-const CACHE = 'darts-v19';
+const CACHE = 'darts-v20';
 const CORE = ['./', 'index.html', 'manifest.json', 'darts-icon-180.png', 'darts-icon-512.png',  './dk-banner.jpg',
   './dk-bg.jpg'
 ,  './dk-side-blue.jpg',
